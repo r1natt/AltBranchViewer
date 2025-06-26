4310842e0676da56409bf625dcf3bc363038b6cd
rinat <rinatt116@gmail.com>
2025-06-26 14:20:52 +0300

    Move validate_branch_name in _types.py

--- core/_types.py
+++ core/_types.py
@@ -4,12 +4,31 @@ from enum import Enum
 
 class BranchName(Enum):
     """Contains branch names"""
-    p11 = "p11"
-    sisyphus = "sisyphus"
+    sisyphus="sisyphus"
+    sisyphus_e2k="sisyphus_e2k"
+    sisyphus_riscv64="sisyphus_riscv64"
+    sisyphus_loongarch64="sisyphus_loongarch64"
+    p11="p11"
+    p10="p10"
+    p10_e2k="p10_e2k"
+    p9="p9"
+    c10f2="c10f2"
+    c9f2="c9f2"
+
+def validate_branch_name(branch_name: str) -> BranchName:
+    if not(isinstance(branch_name, str)):
+        raise TypeError("Branch name must be str or BranchName")
+    try:
+        return BranchName(branch_name)
+    except ValueError:
+        raise ValueError(f"Wrong branch name: {branch_name}. Correct names: {', '.join([bn.value for bn in BranchName])}")
 
 class Version(NamedTuple):
     version: str
     release: str
+
+    def __str__(self):
+        return f"{self.version}-{self.release}"
 
 class PackageInfo(BaseModel):
     name: str
--- core/api.py
+++ core/api.py
@@ -1,26 +1,14 @@
 import json
 import requests
-from requests.exceptions import HTTPError
-import traceback
 from core._types import BranchBinaryPackages, BranchName
 from logger import logger
 
 API_URL = "https://rdb.altlinux.org/api"
 
-def validate_branch_name(branch_name: str) -> BranchName:
-    if not(isinstance(branch_name, str)):
-        raise TypeError("Branch name must be str or BranchName")
-    try:
-        return BranchName(branch_name)
-    except ValueError:
-        raise ValueError(f"Wrong branch name: {branch_name}. Correct names: {', '.join([bn.value for bn in BranchName])}")
-
-def get_branch_binary_packages(branch_name: str) -> BranchBinaryPackages | None:
+def get_branch_binary_packages(branch_name: BranchName) -> BranchBinaryPackages | None:
     """Api request to /export/branch_binry_packages/{branch_name}"""
     try:
-        validated_branch_name = validate_branch_name(branch_name)
-
-        endpoint = API_URL + "/export/branch_binary_packages/" + validated_branch_name.value
+        endpoint = API_URL + "/export/branch_binary_packages/" + branch_name.value
 
         branch_binary_packages = requests.get(endpoint)
         branch_binary_packages.raise_for_status()
